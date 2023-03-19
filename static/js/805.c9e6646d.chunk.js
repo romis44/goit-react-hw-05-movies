58678cead66d06f48d10162acdb720d8b0ec372c
romis44 <110257777+romis44@users.noreply.github.com>
2023-03-19 12:30:51 +0000

    Deploying to gh-pages from @ romis44/goit-react-hw-05-movies@d1bf0ade88842890382933c932d4bce1462f6e8f 🚀

---
+++ static/js/805.c9e6646d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[805],{9805:function(e,n,t){t.r(n),t.d(n,{default:function(){return r}});var u=t(184);function r(){return(0,u.jsx)("h2",{children:"404 Page not found"})}}}]);
+//# sourceMappingURL=805.c9e6646d.chunk.js.map
